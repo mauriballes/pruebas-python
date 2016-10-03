a562c22b004718cd757d925d58727526b882f7f1
Mauricio Ballesteros <mballes95@gmail.com>
2016-10-03 11:43:39 -0400

    Enteros y operadores aritmeticos

---
+++ src/enteros.py
@@ -0,0 +1,26 @@
+e = 5	# Tipo INT
+e = 5L	# Tipo Long
+real = 0.567	# Tipo Float
+real = 0.56e-3	# Numeros con exponentes
+
+# Operadores Aritmeticos
+a = 26
+b = 11.3
+c = 5
+d = 3.5
+# SUMA
+print a + b
+# RESTA
+print c - a
+# MULTIPLICACION
+print d * c
+# EXPONENTE
+print c ** 2
+# DIVISION 
+# (Uno de los operando debe ser float
+# si se quieren decimales)
+print float(c) / a
+# DIVISION ENTERA
+print c / a
+# MODULO
+print 7 % 3
