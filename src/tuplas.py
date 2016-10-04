7b95ec49c3bbb6359a30a0a520daa77ce44d5503
Mauricio Ballesteros <mballes95@gmail.com>
2016-10-04 00:27:53 -0400

    Tuplas

---
+++ src/tuplas.py
@@ -0,0 +1,16 @@
+# Tuplas
+t = (1, True ,"Hola")
+print t
+
+t = (3, "Hola", False)
+print type(t)
+
+#Acceder a elementos
+#Las tuplas hacen los mismos que las listas
+#Pero no se pueden modificar elementos de las
+#Tuplas
+
+ele = t[1]
+print ele
+
+# t[1] = "Hola" No se puede hacer
